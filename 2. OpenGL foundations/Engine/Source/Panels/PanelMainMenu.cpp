3c6415dc4833431da8a9078bbb5f019c9a571ebc
Oscar Pons <ospoga9@gmail.com>
2021-04-26 19:51:03 +0200

    Hi David

--- 2. OpenGL foundations/Engine/Source/Panels/PanelMainMenu.cpp
+++ 2. OpenGL foundations/Engine/Source/Panels/PanelMainMenu.cpp
@@ -13,5 +13,10 @@ void PanelMainMenu::Draw()
 	ImGui::MenuItem("File");
 	ImGui::MenuItem("Edit");
 	ImGui::MenuItem("Help");
+
+
+	char fps[16];
+	sprintf_s(fps, 16, "FPS: %f", 1.0f/App->deltaTime);
+	ImGui::MenuItem(fps);
 	ImGui::EndMainMenuBar();
 }
